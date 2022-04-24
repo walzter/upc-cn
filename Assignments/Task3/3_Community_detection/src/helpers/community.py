83d56340bcfdaccec96b6b840afd612d0a0b2572
“Eric <ewalzthony@gmail.com>
2022-04-24 20:57:12 +0200

    finished ComDetect

--- Assignments/Task3/3_Community_detection/src/helpers/community.py
+++ Assignments/Task3/3_Community_detection/src/helpers/community.py
@@ -1,65 +1,109 @@
-## define a class to hold all the community algorithms 
-import networkx as nx 
+## define a class to hold all the community algorithms
+import networkx as nx
 from .helpers import lol2idx
 
-class CommunityAlgs:
-    def __init__(self, graph:nx.Graph, method:str, verbosity:bool=False) -> None:
+
+class NetworkXCommunityAlgs:
+    def __init__(
+        self, graph: nx.Graph, 
+        method: str,
+        layout: str,
+        params:dict = {'_k':5,'_max_iter':100},
+        verbosity: bool = False,
+    ) -> None:
         self.graph = graph
         self.method = method
         self.mapper = self._get_mapper(self.graph)
         self.verbosity = verbosity
-        self.algorithm = self._get_algorithm(self.graph, self.method) 
-    
+        self.layout = layout
+        self.params = params
+        self.algorithm = self._get_algorithm(self.graph, self.method, self.params)
+
     ## define a function for the Girvan-Newman algorithm
-    def _girvan_newman(self, graph:nx.Graph, verbosity:bool) -> list:
+    def _girvan_newman(
+        self,
+        graph: nx.Graph,
+    ) -> list:
         """
         Calculate the communities using the Girvan-Newman algorithm.
         """
         ## import community module
         from networkx.algorithms.community.centrality import girvan_newman
+
         ## get the communities
         communities = list(girvan_newman(graph))
-        num_coms = len(communities)
-        if verbosity:
-            ## print the number of communities 
-            print(f"Number of communities: {num_coms}")
         ## map the communities to the nodes
         node_colors = [lol2idx(comms) for comms in communities]
         ## return the communities
-        return (communities,node_colors)
-    
+        return (communities, node_colors)
+
     ## define the function for the asyn_fluid algorithm
-    def _asyn_fluid(self, graph:nx.Graph, _k:int=5, _max_iter:int=100) -> list:
+    def _asyn_fluid(self, graph: nx.Graph,params:dict) -> list:
         """
         Calculate the communities using the asyn_fluid algorithm.
         """
         from networkx.algorithms.community import asyn_fluid
-        ## assign fluids 
-        fluids = asyn_fluid.asyn_fluidc(G=graph, k=_k, max_iter=_max_iter)
+
+        ## assign fluids
+        fluids = asyn_fluid.asyn_fluidc(G=graph, k=params['_k'], max_iter=params['_max_iter'])
         ## get the communities
         communities = [fluid for fluid in fluids]
         ## get the node colors
         nc = lol2idx(communities)
         return communities, nc
+
+    ## define function for the Label Propagation algorithm
+    def _label_prop(self, graph: nx.Graph, _max_iter: int = 100) -> list:
+        """
+        Calculate the communities using the Label Propagation algorithm.
+        """
+        from networkx.algorithms.community import label_propagation
+
+        ## get the communities
+        communities = label_propagation.label_propagation_communities(graph)
+        ## get the node colors
+        nc = lol2idx(communities)
+        return communities, nc
+    
+    ## Clauset-Newman-Moore algorithm (Greedy)
+    def _cn_moore(self, graph: nx.Graph, params:dict) -> list:
+        """
+        Calculate the communities using the Clauset-Newman-Moore algorithm.
+        """
+        n_comm = params['n_comm']
+        from networkx.algorithms.community import greedy_modularity_communities
+        communities = greedy_modularity_communities(graph, n_comm)
+        nc = lol2idx(communities)
+        return communities, nc
         
-    ## define the function for getting the algorithm 
-    def _get_algorithm(self, graph:nx.Graph, method:str) -> list:
+        
+
+    @staticmethod
+    def _get_mapper(graph: nx.Graph) -> dict:
+        """
+        Get the mapper from the graph.
+        """
+        ## get the mapper
+        mapper = {name: idx for idx, name in enumerate(graph.nodes())}
+        return mapper
+
+    ## define the function for getting the algorithm
+    def _get_algorithm(self, graph: nx.Graph, method: str,params:dict) -> list:
         """
         Get the algorithm for the community detection.
         """
         ## get the communities
         if method == "girvan_newman":
-            return self._girvan_newman(graph, verbosity=self.verbosity)
+            return self._girvan_newman(graph)
+
         if method == "asyn_fluid":
-            return self._asyn_fluid(graph)
+            return self._asyn_fluid(graph,params)
+
+        if method == "label_prop":
+            return self._label_prop(graph)
+        
+        if method == 'cn_moore':
+            return self._cn_moore(graph,params)
+
         else:
             raise ValueError(f"Method {method} is not supported.")
-    
-    @staticmethod
-    def _get_mapper(graph:nx.Graph) -> dict:
-        """
-        Get the mapper from the graph.
-        """
-        ## get the mapper
-        mapper = {name:idx for idx, name in  enumerate(graph.nodes())}
-        return mapper
--- Assignments/Task3/3_Community_detection/src/helpers/helpers.py
+++ Assignments/Task3/3_Community_detection/src/helpers/helpers.py
@@ -1,21 +1,25 @@
 import glob
-## define the function to load the files 
+import networkx as nx 
+
+## define the function to load the files
 def get_net_files(dir_to_files: str) -> list:
     """
-    get the list of files in the directory 
+    get the list of files in the directory
     """
-    return glob.glob(dir_to_files+"*/*.net")
+    return glob.glob(dir_to_files + "*/*.net")
 
-## define a function that matches the .net file with the corresponing .clu file 
-def get_clu_files(dir_to_files:list) -> dict:
+
+## define a function that matches the .net file with the corresponing .clu file
+def get_clu_files(dir_to_files: list) -> dict:
     """
     Get the .clu files
     """
-    ## get the list of files 
-    return glob.glob(dir_to_files+"*/*.clu")
+    ## get the list of files
+    return glob.glob(dir_to_files + "*/*.clu")
+
 
 ## define function to iterate through a list of files and returns get_file_list
-def get_file_dict(data_directory:str ) -> dict:
+def get_file_dict(data_directory: str) -> dict:
     """
     get the list of files in the directory and store them as a dictionary
     d = {'model_type': file_list}
@@ -25,26 +29,39 @@ def get_file_dict(data_directory:str ) -> dict:
     list_of_nets = get_net_files(data_directory)
     list_of_clus = get_clu_files(data_directory)
     ## find the corresponding .clu file
-    net_clu_pairs = [(file, clu) for file in list_of_nets for clu in list_of_clus \
-                    if file.split('/')[-1].split(".")[0] in clu.split('/')[-1]]
+    net_clu_pairs = [
+        (file, clu)
+        for file in list_of_nets
+        for clu in list_of_clus
+        if file.split("/")[-1].split(".")[0] in clu.split("/")[-1]
+    ]
     ## import collections defaultdict
     from collections import defaultdict
+
     net_clu_dict = defaultdict(list)
-    _ = [net_clu_dict[x[0].split("/")[2].upper()].append(net_clu_pairs[idx]) for idx,x in enumerate(net_clu_pairs)]
+    _ = [
+        net_clu_dict[x[0].split("/")[2].upper()].append(net_clu_pairs[idx])
+        for idx, x in enumerate(net_clu_pairs)
+    ]
     ## dictionaries
     return net_clu_dict
 
+
 ## reading the clu file
-def read_clu(file_path:str) -> list:
+def read_clu(file_path: str) -> list:
     """
     Read the .clu file and return the list of communities.
     """
-    with open(file_path, 'r') as f:
-            return [(idx,int(line.strip())) for idx,line in enumerate(f.readlines()) \
-                                                                    if line[0]!="*"]
-            
-            
-def replace_text_edges(partition:list, mapper:dict) -> list:
+    with open(file_path, "r") as f:
+        return [
+            (idx, int(line.strip()))
+            for idx, line in enumerate(f.readlines())
+            if line[0] != "*"
+        ]
+
+
+##
+def replace_text_edges(partition: list, mapper: dict) -> list:
     """Replaces the text in a list of lists with the corresponding integer"""
     ll = []
     for p in partition:
@@ -52,22 +69,34 @@ def replace_text_edges(partition:list, mapper:dict) -> list:
             new_val = mapper[p[0]]
             ll.append(new_val)
         elif len(p) > 1:
-            l = [] 
-            for i in p: 
+            l = []
+            for i in p:
                 l.append(mapper[i])
             ll.append(l)
     return ll
 
-def lol2idx(lol:list) -> list:
+
+def lol2idx(lol: list) -> list:
     """
     Convert a list of lists to a list of indexers
     """
     d = dict()
-    _ = [d.setdefault(val,idx) for idx, sublist in enumerate(lol) for val in sublist]
+    _ = [d.setdefault(val, idx) for idx, sublist in enumerate(lol) for val in sublist]
     return d
 
-def dict_vals_to_list(d:dict) -> list:
+
+def dict_vals_to_list(d: dict) -> list:
     """
     Convert a dictionary to a list of values
     """
     return list(d.values())
+
+def load_graph_coords(graph: nx.Graph) -> tuple:
+    net = nx.read_pajek(graph)
+    try:
+        coord_x = net.vs['x']
+        coord_y = net.vs['y']
+        pos = [(coord_x[i], coord_y[i]) for i in range(len(coord_x))]
+    except:
+        pos=nx.kamada_kawai_layout(net)
+    return (net, pos)
